ffd608445a8ef8a69550cda32a1736e4ad7ac5db
jack <jack.wooley100@gmail.com>
2023-06-24 00:16:56 -0700

    changed playlist_processor so it returns 3 lists instead of a dict (now includes album info), made good progress on the popularities function

--- song_scores.py
+++ song_scores.py
@@ -22,12 +22,21 @@ def main():
 
     playlist = get_all_playlist_ids(FRIENDS[0], headers)
 
-    song_and_artist_ids = playlist_processor(playlist)
+    song_ids, album_ids, artist_ids = playlist_processor(playlist)
 
-    song_popularities = get_popularities(song_and_artist_ids.keys(), headers)
+    # album_ids = get_all_album_ids(song_and_artist_ids.keys(), headers)
+
+    song_popularities = get_popularities(song_ids, headers, 'tracks', 50)
     
-    artist_popularities = get_popularities(song_and_artist_ids.values(), headers)
+    # artist_popularities = get_popularities(artist_ids, headers, 'artists', 50)  # keeps failing because of too many artists being requested
 
+    album_popularities = get_popularities(album_ids, headers, 'albums', 20)
+
+    # quick check to see song popularity relative to album popularity on this album
+    ### TODO -- come up with a good way of quantifying the relationship between albums and songs
+    summ = sum((np.array(song_popularities) - np.array(album_popularities)) > 5)
+
+    print('swag')
 
     return song_popularities
 
@@ -72,40 +81,42 @@ def playlist_processor(playlist_info):
     ### TODO -- maybe make this two lists instead of a dictionary? 
     # That way you can have a get_artist_popularity function and a get_song-popularity function and it seems to make more sense
     song_ids = []
-    song_and_artist_ids_ = {}
+    album_ids = []
+    artist_ids = []
+    # song_and_artist_ids_ = {}
     for i in range(0, len(songs)):
         song_ids.append(songs[i]['track']['id'])  # add all the song ids to a list
-        # print(songs[i]['track']['name'])
+        album_ids.append(songs[i]['track']['album']['id'])  # add all the album ids to a list
         artists = songs[i]['track']['artists']  # add all the artist ids to a list
-        artist_id_values = []
-        # artist_key = songs[i]
+        artist_id_subsets = []
+        
         for j in range(0, len(artists)):  # double-check this - should it be len(artists) or len(songs)
-            # print(artists[j]['name'])
-            artist_id_values.append(artists[j]['id'])
-            song_and_artist_ids_[songs[i]['track']['id']] = artist_id_values
+            artist_id_subsets.append(artists[j]['id'])
+        
+        artist_ids.append(artist_id_subsets)
 
-    return song_and_artist_ids_
+    return song_ids, album_ids, artist_ids
 
 
-def nest_id_lists(id_list: list):
-    if len(id_list) > 50:  # if the list is longer than 50 members, figure out how many API calls to make
-        if len(id_list) % 50 != 0:
-            how_many_times = (len(id_list) / 50) + 1
+def nest_id_lists(id_list: list, members: int):
+    if len(id_list) > members:  # if the list is longer than `members` items, figure out how many API calls to make
+        if len(id_list) % members != 0:
+            how_many_times = (len(id_list) / members) + 1
         else:
-            how_many_times = len(id_list) / 50
+            how_many_times = len(id_list) / members
         
         start = 0
-        end = 50
+        end = members
 
         nested_list = []
 
         for i in range(0, int(how_many_times)):
             nested_list.append(list(id_list)[start:end])
-            start += 50  # increment to get the next values starting at previous end
-            if end + 50 > len(id_list):
+            start += members  # increment to get the next values starting at previous end
+            if end + members > len(id_list):
                 end = len(id_list)
             else:
-                end += 50
+                end += members
         
         song_ids_ = nested_list
 
@@ -116,19 +127,37 @@ def nest_id_lists(id_list: list):
     return song_ids_
 
 
-def get_popularities(id_list: list, headers_: dict):
+def get_all_album_ids(song_list: list, headers_):
+    # a separate function to get the album_ids for eventually calculating album popularity
 
-    ids_new = nest_id_lists(id_list)
-    
+    ids_new = nest_id_lists(song_list, 20)
     for list_ in ids_new:
-        ### TODO -- loop through new list of lists to get info on 50 tracks at a time
-        subset_of_ids = str(list_).strip('[').strip(']').replace(' ', '')
+        subset_of_ids = str(list_).strip('[\'').strip('\']').replace('\', \'', ',')
+        album_id_raw = requests.get(f'{BASE_URL}tracks/?ids={subset_of_ids}', headers=headers_).json()
+        album_id_clean = album_id_raw['tracks'][0]['id']
 
-        ### TODO -- set up a way to handle whether it's receiving track or artist ids
-        song_pop_short = requests.get(f'{BASE_URL}tracks/{subset_of_ids}', headers=headers_)
-        
-    popularities_ = ''
-    return popularities_
+    return
+
+
+def get_popularities(id_list: list, headers_: dict, endpoint: str, batch_size: int):
+    #  endpoint should be either 'tracks', 'artists', or 'albums'
+    if endpoint not in ['tracks', 'artists', 'albums']:
+        print('Incorrect value entered for endpoint parameter. Make sure it is either "tracks", "artists", or "albums".')
+
+    ids_new = nest_id_lists(id_list, batch_size)
+
+    popularity_list = []
+
+    for list_ in ids_new:
+        subset_of_ids = str(list_).strip('[\'').strip('\']').replace('\', \'', ',').replace('\'], [\'', '\',\'')
+
+        popularities_raw = requests.get(f'{BASE_URL}{endpoint}/?ids={subset_of_ids}', headers=headers_)
+        for j in range(0, len(list_)):
+            popularity_list.append(popularities_raw.json()[endpoint][j]['popularity'])
+    print('swag')
+
+    # popularities_ = ''
+    return popularity_list
 
 
 if __name__=='__main__':
